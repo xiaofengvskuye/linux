0092e6fdeed1788c1e8b844ef3829ae153bb3d9e
Chris Dearman <chris@mips.com>
2008-11-05 16:08:48 -0800

    [MTI] Enable PIIX4 PCI2.1 compliancy on Malta
    Signed-off-by: Chris Dearman <chris@mips.com>

--- arch/mips/mips-boards/malta/malta_setup.c
+++ arch/mips/mips-boards/malta/malta_setup.c
@@ -189,3 +189,15 @@ void __init plat_mem_setup(void)
 	board_time_init = mips_time_init;
 	rtc_mips_get_time = mips_rtc_get_time;
 }
+
+/* Enable PCI 2.1 compatibility in PIIX4 */
+static void __init quirk_dlcsetup(struct pci_dev *dev)
+{
+	  u8 odlc, ndlc;
+	(void) pci_read_config_byte(dev, 0x82, &odlc);
+	/* Enable passive releases and delayed transaction */
+	ndlc = odlc | 7;
+	(void) pci_write_config_byte(dev, 0x82, ndlc);
+}
+DECLARE_PCI_FIXUP_FINAL(PCI_VENDOR_ID_INTEL, PCI_DEVICE_ID_INTEL_82371AB_0,
+			quirk_dlcsetup);
