ab2beb78dd0cdc3cbd3184bd5083b9a6af86941e
Chris Dearman <chris@mips.com>
2008-11-05 16:09:07 -0800

    Display the process tp_value in show_registers
    Signed-off-by: Chris Dearman (chris@mips.com)

--- arch/mips/kernel/traps.c
+++ arch/mips/kernel/traps.c
@@ -307,10 +307,17 @@ void show_regs(struct pt_regs *regs)
 
 void show_registers(struct pt_regs *regs)
 {
+	const int field = 2 * sizeof(unsigned long);
 	show_regs(regs);
 	print_modules();
-	printk("Process %s (pid: %d, threadinfo=%p, task=%p)\n",
-	        current->comm, current->pid, current_thread_info(), current);
+	printk("Process %s (pid: %d, threadinfo=%p, task=%p, tls=%0*lx)\n",
+	       current->comm, current->pid, current_thread_info(), current,
+	       field, current_thread_info()->tp_value);
+	if (cpu_has_userlocal) {
+		unsigned long tls = read_c0_userlocal();
+		if (tls != current_thread_info()->tp_value)
+			printk("*HwTLS: %0*lx\n", field, tls);
+	}
 	show_stacktrace(current, regs);
 	show_code((unsigned int __user *) regs->cp0_epc);
 	printk("\n");
