e0989d669ce6486739525555221a4af692823ea6
Steven J. Hill <sjhill@mips.com>
2011-05-09 14:22:26 -0500

    Add support for M14KEc processor core. Also clean up a few things for future M14K SMTC support.
    Signed-off-by: Steven J. Hill <sjhill@mips.com>

--- arch/mips/oprofile/common.c
+++ arch/mips/oprofile/common.c
@@ -79,6 +79,7 @@ int __init oprofile_arch_init(struct oprofile_operations *ops)
 	switch (current_cpu_type()) {
 	case CPU_5KC:
 	case CPU_14K:
+	case CPU_14KE:
 	case CPU_20KC:
 	case CPU_24K:
 	case CPU_25KF:
