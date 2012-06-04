a5492d3e246616beb0bf91ffeebd0f17ac29d149
Ralf Baechle <ralf@linux-mips.org>
2012-06-04 04:42:04 +0100

    MIPS: PCI: Add missing __devinit attributions to fixup functions.
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    (cherry picked from commit f54d03696bdc1a9457c11ed6ae61e8f001217b25)

--- arch/mips/txx9/generic/pci.c
+++ arch/mips/txx9/generic/pci.c
@@ -307,7 +307,7 @@ static void __devinit quirk_slc90e66_bridge(struct pci_dev *dev)
 	smsc_fdc37m81x_config_end();
 }
 
-static void quirk_slc90e66_ide(struct pci_dev *dev)
+static void __devinit quirk_slc90e66_ide(struct pci_dev *dev)
 {
 	unsigned char dat;
 	int regs[2] = {0x41, 0x43};
@@ -342,7 +342,7 @@ static void quirk_slc90e66_ide(struct pci_dev *dev)
 }
 #endif /* CONFIG_TOSHIBA_FPCIB0 */
 
-static void tc35815_fixup(struct pci_dev *dev)
+static void __devinit tc35815_fixup(struct pci_dev *dev)
 {
 	/* This device may have PM registers but not they are not suported. */
 	if (dev->pm_cap) {
@@ -351,7 +351,7 @@ static void tc35815_fixup(struct pci_dev *dev)
 	}
 }
 
-static void final_fixup(struct pci_dev *dev)
+static void __devinit final_fixup(struct pci_dev *dev)
 {
 	unsigned char bist;
 
