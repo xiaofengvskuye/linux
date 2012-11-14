18997fcf7e637d691e943dcdf0a616085f70039a
Steven J. Hill <sjhill@mips.com>
2012-11-14 12:02:39 -0600

    MIPS: microMIPS: Add breakpoints.
    Redefine BRK_BUG and BRK_KDB values when using a pure microMIPS
    kernel. Add MM_BRK_MEMU for running microMIPS user binaries on
    a standard kernel.
    
    Signed-off-by: Steven J. Hill <sjhill@mips.com>
    (cherry picked from commit 7899fae43e780903e78e9fa510b9371df5559afc)

--- arch/mips/include/asm/break.h
+++ arch/mips/include/asm/break.h
@@ -27,9 +27,16 @@
 #define BRK_STACKOVERFLOW 9	/* For Ada stackchecking */
 #define BRK_NORLD	10	/* No rld found - not used by Linux/MIPS */
 #define _BRK_THREADBP	11	/* For threads, user bp (used by debuggers) */
+
+#ifdef CONFIG_CPU_MICROMIPS
+#define BRK_BUG		12	/* Used by BUG() */
+#define BRK_KDB		13	/* Used in KDB_ENTER() */
+#else
 #define BRK_BUG		512	/* Used by BUG() */
 #define BRK_KDB		513	/* Used in KDB_ENTER() */
+#endif
 #define BRK_MEMU	514	/* Used by FPU emulator */
+#define MM_BRK_MEMU	14	/* Used by FPU emulator for microMIPS */
 #define BRK_KPROBE_BP	515	/* Kprobe break */
 #define BRK_KPROBE_SSTEPBP 516	/* Kprobe single step software implementation */
 #define BRK_MULOVF	1023	/* Multiply overflow */
