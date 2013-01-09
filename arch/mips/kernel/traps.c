83abe1809e84826cce4f2a8da15532f30abf98a9
Leonid Yegoshin <yegoshin@mips.com>
2013-01-09 14:27:09 -0800

    MIPS: bugfix of stack trace dump.
    NMI from YAMON doesn't clear BEV and deliver PC in ErrorPC instead of EPC.
    And it can interrupt a kernel itself and that differs for EVA.
    
    Change-Id: Id9be4ddd9b7d68e70c80fbf8f2a611801106dc7f
    Author: Leonid Yegoshin <yegoshin@mips.com>
    Signed-off-by: Leonid Yegoshin <yegoshin@mips.com>(imported from commit b384a8e3213d1072c5e3225dd437812c3f5b5008)

--- arch/mips/kernel/traps.c
+++ arch/mips/kernel/traps.c
@@ -340,6 +340,7 @@ void show_regs(struct pt_regs *regs)
 void show_registers(struct pt_regs *regs)
 {
 	const int field = 2 * sizeof(unsigned long);
+	mm_segment_t old_fs = get_fs();
 
 	__show_regs(regs);
 	print_modules();
@@ -354,9 +355,12 @@ void show_registers(struct pt_regs *regs)
 			printk("*HwTLS: %0*lx\n", field, tls);
 	}
 
+	if (!user_mode(regs))
+		set_fs(KERNEL_DS);
 	show_stacktrace(current, regs);
 	show_code((unsigned int __user *) regs->cp0_epc);
 	printk("\n");
+	set_fs(old_fs);
 }
 
 static int regs_to_trapnr(struct pt_regs *regs)
@@ -1537,7 +1541,9 @@ void __noreturn nmi_exception_handler(struct pt_regs *regs)
 {
 	raw_notifier_call_chain(&nmi_chain, 0, regs);
 	bust_spinlocks(1);
-	printk("NMI taken!!!!\n");
+	clear_c0_status(ST0_BEV|ST0_NMI);
+	printk("NMI taken, CP0_EPC=%lx (before replacement by CP0_ERROREPC)\n",regs->cp0_epc);
+	regs->cp0_epc = read_c0_errorepc();
 	die("NMI", regs);
 }
 
