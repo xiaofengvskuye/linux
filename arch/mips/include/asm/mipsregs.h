1f867094aa0b1556114dcbe353e7946e1f684fd3
Leonid Yegoshin <yegoshin@mips.com>
2013-04-02 14:01:37 -0700

    MIPS: bugfix: M14Kc support is restored
    Restore support of M14Kc which doesn't have DSP - removed compilation
    flag "-mdsp" and replaced by ".set dsp" in code.
    Bug slipped in commit 0303b20603e466cc0ad25eee95bbc1f6be69c2a7
    
    "MIPS: dsp: Add assembler support for DSP ASEs."
    
    (cherry picked from commit b9950cb5c63fffb5f7d6b57c4882b3a982de0938)
    
    Author: Leonid Yegoshin <yegoshin@mips.com
    Signed-off-by: Leonid Yegoshin <yegoshin@mips.com>(imported from commit 8b8eab5951c8ada42e7a01e4ee973ec93200b417)

--- arch/mips/include/asm/mipsregs.h
+++ arch/mips/include/asm/mipsregs.h
@@ -1284,6 +1284,7 @@ do {									\
 	unsigned int __dspctl;						\
 									\
 	__asm__ __volatile__(						\
+	"       .set    dsp                                     \n"     \
 	"	rddsp	%0, %x1					\n"	\
 	: "=r" (__dspctl)						\
 	: "i" (mask));							\
@@ -1293,30 +1294,31 @@ do {									\
 #define wrdsp(val, mask)						\
 do {									\
 	__asm__ __volatile__(						\
+	"       .set    dsp                                     \n"     \
 	"	wrdsp	%0, %x1					\n"	\
 	:								\
 	: "r" (val), "i" (mask));					\
 } while (0)
 
-#define mflo0() ({ long mflo0; __asm__("mflo %0, $ac0" : "=r" (mflo0)); mflo0;})
-#define mflo1() ({ long mflo1; __asm__("mflo %0, $ac1" : "=r" (mflo1)); mflo1;})
-#define mflo2() ({ long mflo2; __asm__("mflo %0, $ac2" : "=r" (mflo2)); mflo2;})
-#define mflo3() ({ long mflo3; __asm__("mflo %0, $ac3" : "=r" (mflo3)); mflo3;})
+#define mflo0() ({ long mflo0; __asm__(".set    dsp\nmflo %0, $ac0" : "=r" (mflo0)); mflo0;})
+#define mflo1() ({ long mflo1; __asm__(".set    dsp\nmflo %0, $ac1" : "=r" (mflo1)); mflo1;})
+#define mflo2() ({ long mflo2; __asm__(".set    dsp\nmflo %0, $ac2" : "=r" (mflo2)); mflo2;})
+#define mflo3() ({ long mflo3; __asm__(".set    dsp\nmflo %0, $ac3" : "=r" (mflo3)); mflo3;})
 
-#define mfhi0() ({ long mfhi0; __asm__("mfhi %0, $ac0" : "=r" (mfhi0)); mfhi0;})
-#define mfhi1() ({ long mfhi1; __asm__("mfhi %0, $ac1" : "=r" (mfhi1)); mfhi1;})
-#define mfhi2() ({ long mfhi2; __asm__("mfhi %0, $ac2" : "=r" (mfhi2)); mfhi2;})
-#define mfhi3() ({ long mfhi3; __asm__("mfhi %0, $ac3" : "=r" (mfhi3)); mfhi3;})
+#define mfhi0() ({ long mfhi0; __asm__(".set    dsp\nmfhi %0, $ac0" : "=r" (mfhi0)); mfhi0;})
+#define mfhi1() ({ long mfhi1; __asm__(".set    dsp\nmfhi %0, $ac1" : "=r" (mfhi1)); mfhi1;})
+#define mfhi2() ({ long mfhi2; __asm__(".set    dsp\nmfhi %0, $ac2" : "=r" (mfhi2)); mfhi2;})
+#define mfhi3() ({ long mfhi3; __asm__(".set    dsp\nmfhi %0, $ac3" : "=r" (mfhi3)); mfhi3;})
 
-#define mtlo0(x) __asm__("mtlo %0, $ac0" ::"r" (x))
-#define mtlo1(x) __asm__("mtlo %0, $ac1" ::"r" (x))
-#define mtlo2(x) __asm__("mtlo %0, $ac2" ::"r" (x))
-#define mtlo3(x) __asm__("mtlo %0, $ac3" ::"r" (x))
+#define mtlo0(x) __asm__(".set    dsp\nmtlo %0, $ac0" ::"r" (x))
+#define mtlo1(x) __asm__(".set    dsp\nmtlo %0, $ac1" ::"r" (x))
+#define mtlo2(x) __asm__(".set    dsp\nmtlo %0, $ac2" ::"r" (x))
+#define mtlo3(x) __asm__(".set    dsp\nmtlo %0, $ac3" ::"r" (x))
 
-#define mthi0(x) __asm__("mthi %0, $ac0" ::"r" (x))
-#define mthi1(x) __asm__("mthi %0, $ac1" ::"r" (x))
-#define mthi2(x) __asm__("mthi %0, $ac2" ::"r" (x))
-#define mthi3(x) __asm__("mthi %0, $ac3" ::"r" (x))
+#define mthi0(x) __asm__(".set    dsp\nmthi %0, $ac0" ::"r" (x))
+#define mthi1(x) __asm__(".set    dsp\nmthi %0, $ac1" ::"r" (x))
+#define mthi2(x) __asm__(".set    dsp\nmthi %0, $ac2" ::"r" (x))
+#define mthi3(x) __asm__(".set    dsp\nmthi %0, $ac3" ::"r" (x))
 
 #else
 
