733a85d1bbe7df644b0dcd94eef073ee04ec1c0c
James Hogan <james.hogan@imgtec.com>
2014-01-27 17:26:58 +0000

    MIPS: OProfile: Add CPU_PROAPTIV case
    OProfile never used the MIPS model for CPU_PROAPTIV, even though a cpu
    type string was defined for it. Add a CPU_PROAPTIV case for the MIPS
    model.
    
    Signed-off-by: James Hogan <james.hogan@imgtec.com>

--- arch/mips/oprofile/common.c
+++ arch/mips/oprofile/common.c
@@ -87,6 +87,7 @@ int __init oprofile_arch_init(struct oprofile_operations *ops)
 	case CPU_74K:
 	case CPU_1074K:
 	case CPU_LOONGSON1:
+	case CPU_PROAPTIV:
 	case CPU_INTERAPTIV:
 	case CPU_M5150:
 	case CPU_SB1:
