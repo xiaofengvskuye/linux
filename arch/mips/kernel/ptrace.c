b65210b49ca8f74017eee7630785f377717ad7d7
Markos Chandras <markos.chandras@imgtec.com>
2014-01-29 14:52:31 +0000

    MIPS: asm: syscall: Define syscall_get_arch
    This effectively renames __syscall_get_arch to syscall_get_arch
    and implements a compatible interface for the seccomp API.
    The seccomp code (kernel/seccomp.c) expects a syscall_get_arch
    function to be defined for every architecture, so we drop
    the leading underscores from the existing function.
    
    This also makes use of the 'task' argument to determine the type
    the process instead of assuming the process has the same
    characteristics as the kernel it's running on.
    
    Reviewed-by: Paul Burton <paul.burton@imgtec.com>
    Reviewed-by: James Hogan <james.hogan@imgtec.com>
    Signed-off-by: Markos Chandras <markos.chandras@imgtec.com>
    
    Signed-off-by: Markos Chandras <markos.chandras@imgtec.com>

--- arch/mips/kernel/ptrace.c
+++ arch/mips/kernel/ptrace.c
@@ -678,7 +678,7 @@ asmlinkage void syscall_trace_enter(struct pt_regs *regs)
 	}
 
 out:
-	audit_syscall_entry(__syscall_get_arch(),
+	audit_syscall_entry(syscall_get_arch(current, regs),
 			    regs->regs[2],
 			    regs->regs[4], regs->regs[5],
 			    regs->regs[6], regs->regs[7]);
