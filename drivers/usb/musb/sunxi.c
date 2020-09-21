ee8b23693d90cd4c86aad08df58bc9af59aac402
jack-marks <874364169@qq.com>
2020-09-21 18:18:58 +0800

    modify usb ep count

--- drivers/usb/musb/sunxi.c
+++ drivers/usb/musb/sunxi.c
@@ -644,6 +644,18 @@ static struct musb_fifo_cfg sunxi_musb_mode_cfg_h3[] = {
 	MUSB_EP_FIFO_SINGLE(4, FIFO_RX, 512),
 };
 
+/* SUNIV OTG supports only 3 endpoints */
+#define SUNXI_MUSB_MAX_EP_NUM_SUNIV	4
+
+static struct musb_fifo_cfg sunxi_musb_mode_cfg_suniv[] = {
+	MUSB_EP_FIFO_SINGLE(1, FIFO_TX, 512),
+	MUSB_EP_FIFO_SINGLE(1, FIFO_RX, 512),
+	MUSB_EP_FIFO_SINGLE(2, FIFO_TX, 512),
+	MUSB_EP_FIFO_SINGLE(2, FIFO_RX, 512),
+	MUSB_EP_FIFO_SINGLE(3, FIFO_TX, 512),
+	MUSB_EP_FIFO_SINGLE(3, FIFO_RX, 512),
+};
+
 static const struct musb_hdrc_config sunxi_musb_hdrc_config = {
 	.fifo_cfg       = sunxi_musb_mode_cfg,
 	.fifo_cfg_size  = ARRAY_SIZE(sunxi_musb_mode_cfg),
@@ -662,6 +674,14 @@ static struct musb_hdrc_config sunxi_musb_hdrc_config_h3 = {
 	.ram_bits	= SUNXI_MUSB_RAM_BITS,
 };
 
+static struct musb_hdrc_config sunxi_musb_hdrc_config_suniv = {
+	.fifo_cfg       = sunxi_musb_mode_cfg_suniv,
+	.fifo_cfg_size  = ARRAY_SIZE(sunxi_musb_mode_cfg_suniv),
+	.multipoint	= true,
+	.dyn_fifo	= true,
+	.num_eps	= SUNXI_MUSB_MAX_EP_NUM_SUNIV,
+	.ram_bits	= SUNXI_MUSB_RAM_BITS,
+};
 
 static int sunxi_musb_probe(struct platform_device *pdev)
 {
@@ -705,10 +725,11 @@ static int sunxi_musb_probe(struct platform_device *pdev)
 		return -EINVAL;
 	}
 	pdata.platform_ops	= &sunxi_musb_ops;
-	if (!of_device_is_compatible(np, "allwinner,sun8i-h3-musb"))
-		pdata.config = &sunxi_musb_hdrc_config;
-	else
-		pdata.config = &sunxi_musb_hdrc_config_h3;
+	pdata.config = &sunxi_musb_hdrc_config;
+	if (of_device_is_compatible(np, "allwinner,sun8i-h3-musb"))
+ 		pdata.config = &sunxi_musb_hdrc_config_h3;
+	if (of_device_is_compatible(np, "allwinner,suniv-f1c100s-musb"))
+		pdata.config = &sunxi_musb_hdrc_config_suniv;
 
 	glue->dev = &pdev->dev;
 	INIT_WORK(&glue->work, sunxi_musb_work);
